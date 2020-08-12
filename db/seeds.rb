052c7bb785d9fddae517081d918ad0136e2bd5ec
Tom Renard <tom.renard@zoho.com>
2020-08-12 12:53:09 +0200

    controllers mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating some restaurants..."
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    category: categories.sample
+    )
+  puts "Restaurant with id #{restaurant.id} was created"
+
+end
+
+puts "Created #{Restaurant.count} restaurants"
